95d115b42b5015460ad1303778cf7af9aa4cf7d3
Mohammed Saleh <mohammedsalehbtw@gmail.com>
2024-07-11 02:36:57 +0300

    first commit of the tree that generated from the dump

---
+++ samsung/a13ve/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a13ve.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a13ve-user \
+    omni_a13ve-userdebug \
+    omni_a13ve-eng
